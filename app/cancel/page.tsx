4c43abe0e89f8badfdadb2cf6612534341260a93
hassanuahmad <ahmad12p@uwindsor.ca>
2023-11-26 13:49:10 -0500

    cart get emptied out after buy

--- app/cancel/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function StripeCancel() {
-    return (
-        <>
-            <div>
-                <h1>Payment Cancel</h1>
-            </div>
-        </>
-    )
-}
--- app/success/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function StripeSuccess() {
-    return (
-        <>
-            <div>
-                <h1>Payment Successful</h1>
-            </div>
-        </>
-    )
-}
