4cf93f8248fd2be4af0aeded284a2d27d8b86802
dreamdevfast <dream.dev1215@gmail.com>
2023-12-29 05:09:37 +0300

    Fix number 2

--- types/shared/user.d.ts
+++ types/shared/user.d.ts
@@ -1,26 +1,27 @@
 export interface UserInfo {
-    client_id?: string;
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: string;
-    country?: string;
+  client_id?: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  country?: string;
 }
 
 export interface KtererSpecificInfo {
-    apartment?: string;
-    bio?: string;
-    city: string;
-    country: string;
-    ethnicity: string;
-    experienceUnit: number;
-    experienceValue: string;
-    postal_code: string;
-    profile_image_url: string;
-    province: string;
-    street_address: string;
+  id: number;
+  apartment?: string;
+  bio?: string;
+  city: string;
+  country: string;
+  ethnicity: string;
+  experienceUnit: number;
+  experienceValue: string;
+  postal_code: string;
+  profile_image_url: string;
+  province: string;
+  street_address: string;
 }
 
 export interface KtererInfo extends UserInfo {
-    kterer: Partial<KtererSpecificInfo>;
+  kterer: Partial<KtererSpecificInfo>;
 }
