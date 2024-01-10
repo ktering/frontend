d4a66a41e4aef0b94c1ad2f62da50a93dbc32ef0
hassanuahmad <ahmad12p@uwindsor.ca>
2024-01-09 21:52:42 -0500

    updated logos and updated orders page

--- app/consumer/layout.tsx
+++ app/consumer/layout.tsx
@@ -1,29 +1,38 @@
 import React from "react";
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import UserNavbar from "@/components/userNavbar";
-import { CartProvider } from "@/components/cartContext";
-import { NotificationProvider } from "@/components/notificationContext";
+import {CartProvider} from "@/components/cartContext";
+import {NotificationProvider} from "@/components/notificationContext";
+import {currentUser} from "@clerk/nextjs";
+import KtererDashboardNavbar from "@/components/ktererDashboardNavbar";
 
 export const metadata: Metadata = {
-  title: "Kterings | Profile",
-  description: "Kterings Profile Page",
+    title: "Kterings | Profile",
+    description: "Kterings Profile Page",
 };
 
-export default function ConsumerLayout({
-  children,
-}: {
-  children: React.ReactNode;
+export default async function ConsumerLayout({
+                                           children,
+                                       }: {
+    children: React.ReactNode;
 }) {
-  return (
-    <CartProvider>
-      <NotificationProvider>
-        <section>
-          <UserNavbar />
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-            {children}
-          </div>
-        </section>
-      </NotificationProvider>
-    </CartProvider>
-  );
+    const user = await currentUser();
+    if (!user) {
+        return null;
+    }
+    const isKterer = user?.publicMetadata?.ktererSignUpCompleted === true;
+
+    return (
+        <CartProvider>
+            <NotificationProvider>
+                <div>
+                    <UserNavbar/>
+                    {isKterer ? <KtererDashboardNavbar/> : null}
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        {children}
+                    </div>
+                </div>
+            </NotificationProvider>
+        </CartProvider>
+    );
 }
