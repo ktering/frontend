27dfcaa4af3338250eed3cabd1e43f0fcfa76706
Daruin Herrera <153112202+DaruinHerreraIgniweb@users.noreply.github.com>
2024-05-03 17:31:52 -0500

    fix access routes kterer
    Validation is modified to allow access to the kterer routes.

--- contexts/CartContext.tsx
+++ contexts/CartContext.tsx
@@ -1,42 +1,46 @@
 "use client";
 
-import React, { createContext, useState, ReactNode } from 'react';
+import React, { createContext, useState, ReactNode } from "react";
 
 interface CartContextType {
-    cartCount: number;
-    updateCartCount: (newCount: number) => void;
+  cartCount: number;
+  updateCartCount: (newCount: number) => void;
 }
 
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 interface CartProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const useCartCount = () => {
-    const context = React.useContext(CartContext);
-    if (context === undefined) {
-        throw new Error('useCart must be used within a CartProvider');
-    }
-    return context;
-}
+  const context = React.useContext(CartContext);
+  if (context === undefined) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+};
 
 export const CartProvider: React.FC<CartProviderProps> = ({ children }) => {
-    const [cartCount, setCartCount] = useState<number>(() => {
-        const cart = localStorage.getItem('cart');
-        if (cart) {
-            return JSON.parse(cart).length;
-        }
-        return 0;
-    });
+  const [cartCount, setCartCount] = useState<number>(() => {
+    const cart =
+      typeof localStorage !== "undefined"
+        ? JSON.parse(localStorage.getItem("cart") || "[]").length
+        : 0;
 
-    const updateCartCount = (newCount: number) => {
-        setCartCount(newCount);
-    };
+    if (cart) {
+      return cart;
+    }
+    return 0;
+  });
 
-    return (
-        <CartContext.Provider value={{ cartCount, updateCartCount }}>
-            {children}
-        </CartContext.Provider>
-    );
+  const updateCartCount = (newCount: number) => {
+    setCartCount(newCount);
+  };
+
+  return (
+    <CartContext.Provider value={{ cartCount, updateCartCount }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
