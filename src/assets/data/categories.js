714e9da9a92a76cff46fc6e6d0495be8251458da
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-07-21 00:02:54 +0500

    Setup data for category

---
+++ src/assets/data/categories.js
@@ -0,0 +1,19 @@
+// src/data/categories.js
+
+// import trendingImg from '../assets/category-images/trending.png';
+import asianImg from '';
+import middleEasternImg from '../assets/category-images/middle-eastern.png';
+import vegetarianImg from '../assets/category-images/vegetarian.png';
+import veganImg from '../assets/category-images/vegan.png';
+import dessertsImg from '../assets/category-images/desserts.png';
+
+const categories = [
+  { id: 'trending', name: 'Trending', image: trendingImg },
+  { id: 'asian', name: 'Asian', image: asianImg },
+  { id: 'middle-eastern', name: 'Middle Eastern', image: middleEasternImg },
+  { id: 'vegetarian', name: 'Vegetarian', image: vegetarianImg },
+  { id: 'vegan', name: 'Vegan', image: veganImg },
+  { id: 'desserts', name: 'Desserts', image: dessertsImg },
+];
+
+export default categories;
