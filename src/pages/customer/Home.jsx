627ea64bf67ff272d9d80b15eea802ffc6e538fd
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-07-21 21:24:35 +0500

    added responsivess for about section

--- src/pages/customer/Home.jsx
+++ src/pages/customer/Home.jsx
@@ -6,18 +6,18 @@ import AboutSection from "../../components/customer/home/AboutSection";
 export default function Home() {
 
     return (
-        <div>
+        <>
             <Header />
-            <div className="div  mt-8 w-4/5 mx-auto">
+            <div className="div mt-8 w-4/5 mx-auto">
                 <Hero />
-            <section className="w-full px-4 py-10 max-w-6xl lg:max-w-[86%] mx-auto">
+                <section className="w-full px-4 py-10">
                 <h2 className="text-2xl font-bold text-center mb-8">Explore Homemade Food</h2>
                 <CategorySection />
                 <FoodGridSection limit={8} showButton={true} />
             </section>
             <AboutSection />
+            </div>
 
-        </div>
-        </div>
+        </>
     );
 }
