e9f7ee3223c1d732e92c6e1636f708ccaa46c01b
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-07-26 00:49:12 +0500

    Added dish page

--- src/components/customer/home/CategorySection.jsx
+++ src/components/customer/home/CategorySection.jsx
@@ -1,53 +1,3 @@
-// import CategoryItem from './CategoryItem';
-// import categories from '../../../assets/data/categories';
-// import { Link } from 'react-router-dom';
-
-// import { Flame, Salad, CakeSlice } from "lucide-react";
-// import { GiCarrot } from "react-icons/gi";
-// import { FaBowlRice } from "react-icons/fa6";
-// import { MdKebabDining } from "react-icons/md"; // ✅ for Middle Eastern
-
-// const iconsMap = {
-//   trending: Flame,
-//   asian: FaBowlRice,
-//   "middle-eastern": MdKebabDining,
-//   vegetarian: GiCarrot,
-//   vegan: Salad,
-//   desserts: CakeSlice,
-// };
-
-// const CategorySection = () => {
-//   return (
-//     <div className="w-full px-4 mb-8">
-
-//       <div className="flex flex-wrap justify-center gap-2 sm:gap-3 md:gap-4">
-//         {categories.map((cat) => {
-//           const Icon = iconsMap[cat.id];
-//           return (
-//             <CategoryItem
-//               key={cat.id}
-//               name={cat.name}
-//               image={cat.image}
-//               icon={Icon}
-//             />
-//           );
-//         })}
-//       </div>
-
-//           <div className="sm:hidden text-center mt-4">
-//   <Link to="/categories">
-//     <button className="bg-primary hover:bg-primary/90 text-white text-sm font-medium px-6 py-2 rounded-full transition-transform duration-200 transform hover:scale-105 active:scale-95">
-//       Explore More Categories
-//     </button>
-//   </Link>
-// </div>
-
-//     </div>
-//   );
-// };
-
-// export default CategorySection;
-
 import { useState } from 'react';
 import CategoryItem from './CategoryItem';
 import categories from '../../../assets/data/categories';
@@ -95,13 +45,13 @@ const CategorySection = ({ onSelectCategory, selected }) => {
         })}
       </div>
 
-      <div className="sm:hidden text-center mt-4">
+      {/* <div className="sm:hidden text-center mt-4">
         <Link to="/categories">
           <button className="bg-primary hover:bg-primary/90 text-white text-sm font-medium px-6 py-2 rounded-full transition-transform duration-200 transform hover:scale-105 active:scale-95">
             Explore More Categories
           </button>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };
