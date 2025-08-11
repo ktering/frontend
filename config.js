88363d61c51c8faba27543159a6ac9edcf198d5a
Muzdalfa Zulfiqar <muzdalfazulfiqar11@gmail.com>
2025-08-11 19:24:23 +0500

    Updated backend url

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // src/api/config.js
-// export const API_BASE = "http://localhost:5000" || import.meta.env.VITE_BACKEND_URL;
-export const API_BASE = "http://localhost:5000";
+export const API_BASE =  import.meta.env.VITE_BACKEND_URL;
