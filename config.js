29951332393d789993f301232e3a7b87b44c22d7
Muzdalfa Zulfiqar <159936635+MuzdalfaZulfiqar@users.noreply.github.com>
2025-08-15 00:13:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // src/api/config.js
-// export const API_BASE =  import.meta.env.VITE_BACKEND_URL;
-export const API_BASE =  "http://localhost:5000";
+export const API_BASE =  import.meta.env.VITE_BACKEND_URL;
 
 
