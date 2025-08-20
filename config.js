5c1cf0eaf0580e7aa3ccc3070163c7f678bf6c0a
Khaula Nauman <114153459+khaulanauman@users.noreply.github.com>
2025-08-20 23:31:21 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// src/api/config.js
-// export const API_BASE =  import.meta.env.VITE_BACKEND_URL;
-export const API_BASE =  "http://localhost:5000"; // Fallback for local development
+export const API_BASE =  import.meta.env.VITE_BACKEND_URL;
